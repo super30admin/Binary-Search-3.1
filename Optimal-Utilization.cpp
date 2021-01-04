d7e374cc5a2b139ecf767b357b3946aae38603c6
Giridhar <4182759+girisub@users.noreply.github.com>
2021-01-04 14:16:53 -0500

    Add files via upload

---
+++ Optimal-Utilization.cpp
@@ -0,0 +1,69 @@
+//Time - O(nlogn) + O(mlogm) + O(min(m.n)log(max(m,n)))
+//Space - O(m+n)
+class Solution{
+public:
+	vector<pair<int,int>> getIds(vector<pair<int,int>> a, vector<pair<int,int>> b, int target){
+
+		vector<pair<int,int>> ret;
+
+		if(a.size() == 0 || b.size() ==0) return ret;
+		
+		vector<pair<int,int>> small = a;
+		vector<pair<int,int>> large = b;
+		if(a.size()>b.size()) {
+			large = a;
+			small = b;
+		}
+
+		sort(large.begin(),large.end(),[](const pair<int,int>& x, const pair<int,int>& y){
+			return x.second<y.second;
+		});
+
+		sort(small.begin(),small.end(),[](const pair<int,int>& x, const pair<int,int>& y){
+			return x.second<y.second;
+		});
+
+
+		int minDiff = INT_MAX;
+		for(int i = 0;i<small.size();i++){
+			int idx = binarySearch(large, target-small[i].second);
+			int newDiff = target-(small[i].second + large[idx].second);
+
+			if(newDiff>=0 && newDiff <= minDiff){
+				if(newDiff<minDiff){
+					minDiff = newDiff;
+					ret.erase();
+				}
+				ret.push_back(make_pair(small[i].first,large[idx].first));
+				int index = idx+1;
+				while(index<large.size() && large[index].second == large[idx].second){
+					ret.push_back(make_pair(small[i].first,large[index].first));
+				}
+			}
+
+		}
+		return ret;
+
+	}
+
+
+	int binarySearch(vector<pair<int,int>>& arr, int ele){
+		int l = 0, h = arr.size()-1;
+
+		while(l+1<r){
+			int m = l+(r-l)/2;
+
+			if(arr[m].second == ele){
+				int left = m;
+				while(left >0 && arr[m].second == arr[left-1]) left--;
+				return left;
+			}else if(arr[m].second < ele){
+				l = m;
+			}else{
+				r = m;
+			}
+		}
+
+		return l;
+	}
+};
