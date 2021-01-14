72a0505a8af77a2792973cfeea5f61ef24552561
Rishi Raj <rishiraj824@gmail.com>
2021-01-14 00:31:02 -0500

    Update h-index.java

--- h-index.java
+++ h-index.java
@@ -1,3 +1,32 @@
+// H Index
+// Time - O(lg N)
+// Space - O(1)
+class Solution {
+    public int hIndex(int[] citations) {
+        if(citations == null || citations.length == 0) {
+            return 0;
+        }
+        int n = citations.length;
+        int high = n - 1;
+        int low = 0;
+        
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if(citations[mid] == n - mid) {
+                return n - mid;
+            }
+            else if(citations[mid] > n - mid) {
+                high = mid - 1;
+            }
+            else if(citations[mid] < n - mid){
+                low = mid + 1;
+            }
+        }        
+        
+        return n - low;
+    }
+} 
 
 // Brute Force 
 // Time - O(N)
