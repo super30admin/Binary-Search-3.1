2265c2381c02d2cc5fe25a1d8b6f8df9492f01e6
Rishi Raj <rishiraj824@gmail.com>
2021-01-13 22:53:10 -0500

    Create h-index.java

---
+++ h-index.java
@@ -0,0 +1,22 @@
+
+// Brute Force 
+// Time - O(N)
+// Space - O(1)
+
+class Solution {
+    public int hIndex(int[] citations) {
+        if(citations == null || citations.length == 0) {
+            return 0;
+        }
+        
+        int n = citations.length;
+        for(int i=0;i<n;i++) {
+            int diff = n - i;
+            if(citations[i]>=diff) {
+                  return diff;
+            }
+        }
+        
+        return 0;
+    }
+}
