33fd63cc001f70dc03b91096751ce62327f7fad4
amby2964 <75859704+amby2964@users.noreply.github.com>
2021-02-07 13:54:14 -0700

    Update HIndex2.py

--- HIndex2.py
+++ HIndex2.py
@@ -1,1 +1,20 @@
 
+class Solution:
+    '''
+    Complexity:
+    TIme: O(logn)
+    Space: O(1)
+    '''
+    def hIndex(self, citations: List[int]) -> int:
+        low = 0
+        high = len(citations)-1
+        N = len(citations)
+        k = 0
+        while(high>=0 and low<len(citations) and low<=high):
+            mid = (low+high)//2
+            if(citations[mid]>=N-mid):
+                k = N-mid
+                high = mid - 1
+            else:
+                low = mid+1  
+        return k
