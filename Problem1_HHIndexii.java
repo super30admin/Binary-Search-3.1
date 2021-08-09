304e5223c711017f6053ae276e6a88d49a8e0682
sahithpintu <sahithcricketer@gmail.com>
2021-08-08 20:43:43 -0500

    Completed Binary Search - 3.1 Review

---
+++ Problem1_HHIndexii.java
@@ -0,0 +1,31 @@
+// Time Complexity : o(logn)
+// Space Complexity : o(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public int hIndex(int[] citations) {
+        
+        if(citations==null||citations.length==0)
+            return 0;
+        int n=citations.length;
+        int low=0;
+        int high=n-1;
+        
+        while(low<=high)
+        {
+            int mid=low + (high-low)/2;
+            
+            if(citations[mid]==(n-mid))
+                return n-mid;
+            else if(citations[mid]<(n-mid))
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return n-low; // when low crosses high other wise we can return n-mid by declaring mid variable outside
+    }
+}
