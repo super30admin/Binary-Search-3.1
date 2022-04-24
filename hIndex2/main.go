ffb9ae5f03132050b37c3320434448fd99baaf6c
amritgill27195 <100459226+amritgill27195@users.noreply.github.com>
2022-04-24 16:22:08 -0400

    Update main.go

--- hIndex2/main.go
+++ hIndex2/main.go
@@ -1,19 +1,45 @@
 // worst problem desc ever...
-// time: o(logn)
-// space: o(1)
+// brute force
+// doing this question brute force first makes me understand why binary search helps...
+// time: o(c) where c is the number of citations
+// func hIndex(citations []int) int { 
+//     n := len(citations)
+//     for i := 0; i < len(citations); i++ {
+//         diff := n-i
+//         if diff <= citations[i] {
+//             return diff
+//         }
+//     }
+//     return 0
+// }
+
+
+/*
+    approach: binary search
+    - basically search for the same EXACT thing we searched for in brute force
+    - if n-idx <= citations[idx]
+        - in brute force ( linear traverse, we returned right away because its the first element from left side )
+        - but in binary search, we will save it as it may NOT be the first element and so we will save it and search left
+    - otherwise
+        - search right
+    
+time: o(logn)
+space: o(1)
+
+*/
 func hIndex(citations []int) int {
     left := 0
-    right := len(citations)-1
+    hIdx := 0
     n := len(citations)
+    right := len(citations)-1
     for left <= right {
-        mid := left+(right-left)/2
-        if n-mid == citations[mid] {
-            return n-mid
-        } else if citations[mid] > n-mid {
+        mid := left + (right-left)/2
+        if n-mid <= citations[mid] {
+            hIdx = n-mid
             right = mid-1
         } else {
             left = mid+1
         }
     }
-    return n-left
+    return hIdx
 }
