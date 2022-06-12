b6ba38ceee1364bd76bf753e4c99c92d8b0e25f5
sureshgoudmula <91751941+sureshgoudmula@users.noreply.github.com>
2022-06-12 11:08:51 -0700

    Create HIndex.java

---
+++ HIndex.java
@@ -0,0 +1,19 @@
+class HIndex {
+    public int hIndex(int[] citations) {
+        int len = citations.length;
+        int low = 0;
+        int high = len -1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            int diff = len -mid;
+            if(citations[mid] ==diff) return diff;
+            else if(citations[mid] > diff){
+                high = mid - 1;
+            }
+            else{
+                low = mid +1;
+            }
+        }
+        return len - low;
+    }
+}
