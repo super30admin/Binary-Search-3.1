4810ad194e7c758a184663c7fbddb39f23f617cc
Mani9333 <manikanta.ellenki9@gmail.com>
2022-06-15 14:15:54 -0700

    Comitted

--- Construct Binary Tree from Preorder and Inorder Traversal (Leet 105).py
+++ Construct Binary Tree from Preorder and Inorder Traversal (Leet 105).py
@@ -37,3 +37,40 @@ class Solution:
 
 
 
+# Binary Search (Efficient)
+# Time: O(n)
+# Space: O(n)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    index = 0
+    hm = dict()
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        self.hm = dict()
+        self.index = 0
+        for i in range(len(inorder)):
+            self.hm[inorder[i]] = i
+        
+        
+        return self.helper(preorder, 0, len(inorder)-1)
+    
+    def helper(self, preorder, start, end):
+        if start > end:
+            return None
+        
+        rootval = preorder[self.index]
+        self.index += 1
+        rootindex = self.hm[rootval]
+        
+        root = TreeNode(rootval)
+        
+        root.left = self.helper(preorder, start, rootindex-1)
+        root.right = self.helper(preorder, rootindex+1, end)
+        return root
+
+
