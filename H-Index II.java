024b2e3288173387da242e6fee2a258659cdc998
Dishant Janakbhai Patel <39313255+dishantpatel98@users.noreply.github.com>
2024-07-27 02:12:08 -0500

    Create H-Index II.java

---
+++ H-Index II.java
@@ -0,0 +1,26 @@
+
+//TC = O(logn)
+class Solution {
+    public int hIndex(int[] citations) {
+        if(citations == null || citations.length == 0) return 0;
+        int n = citations.length;
+        int low = 0, high = n - 1;
+        while(low <= high)
+        {
+            int mid = low + (high-low) / 2;
+            if(citations[mid] == n - mid)
+            {
+                return n - mid;
+            }
+            else if(citations[mid] > n - mid)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return n - low;
+    }
+}
