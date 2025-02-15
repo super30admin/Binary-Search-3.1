88dfd61c6596baf8e22b0c89c5ce0bb62177cdf5
tanish-gupta1 <tanishpargal098765@gmail.com>
2025-02-15 23:56:38 +0530

    Binary-Search-3.1 : Problem2 completed.

---
+++ Problem2.java
@@ -0,0 +1,24 @@
+public class Problem2 {
+    class Solution {
+        //t.c -> O(logn)
+        //s.c -> O(1)
+        public int hIndex(int[] citations) {
+            if(citations == null || citations.length == 0){
+                return 0;
+            }
+            int n =  citations.length;
+            int low = 0;
+            int high = n-1;
+            while(low<=high){
+                int mid = low+(high-low)/2;
+                if(citations[mid] >= n - mid){
+                    high = mid-1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            return n - low;
+        }
+    }
+}
